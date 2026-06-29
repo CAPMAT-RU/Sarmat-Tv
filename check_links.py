ba2530ea9e0024bbb8db754976fccf1d2eaaa531
CAPMAT-RU <sarmatiandon@gmail.com>
2026-06-30 01:29:28 +0300

    check_links.py

--- check_links.py
+++ check_links.py
@@ -9,8 +9,7 @@ workflow_dispatch:
 permissions:
 contents: write
 
-jobs:
-build:
+jobs: build:
 runs-on: ubuntu-latest
 steps:
 - name: Checkout code
@@ -21,13 +20,10 @@ uses: actions/setup-python@v4
 with:
 python-version: '3.9'
 
-- name: Install dependencies
-run: pip install requests
+- name: Install dependencies run: pip install requests
 
 - name: Run script
-run: python check_links.py
-
-- name: Commit changes
+run: python check_links.py - name: Commit changes
 run: |
 git config --global user.name 'github-actions[bot]'
 git config --global user.email 'github-actions[bot]@users.noreply.github.com'
